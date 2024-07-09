84e1b5310c2c121386044dd9f4ccac84e43d51d7
ASP73 <alecsparker@outlook.com>
2024-07-09 03:43:03 +0100

    seeds database

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env.test" });
+require("dotenv").config({ path: "../../.env.test" });
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_PORT:", process.env.DB_PORT);
 console.log("DB_USER:", process.env.DB_USER);
--- db/seeds/seeds.js
+++ db/seeds/seeds.js
@@ -11,216 +11,192 @@ const seed = ({
   apostrophesData,
 }) => {
   return db
-    .query(`DROP TABLE IF EXISTS responses;`)
+    .query(`DROP TABLE IF EXISTS attempts;`)
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS fixtures;`);
+      return db.query(`DROP TABLE IF EXISTS users;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS quiz;`);
     })
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS roles;`);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS users;`);
+      return db.query(`DROP TABLE IF EXISTS aoran;`);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS teams;`);
+      return db.query(`DROP TABLE IF EXISTS anoption;`);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS venues;`);
+      return db.query(`DROP TABLE IF EXISTS apostrophes;`);
     })
     .then(() => {
       return db.query(`
-        CREATE TABLE venues (
-          venue_id SERIAL PRIMARY KEY,
-          venue_name VARCHAR NOT NULL,
-          venue_address_1 VARCHAR NOT NULL,
-          venue_address_2 VARCHAR NOT NULL,
-          venue_postcode VARCHAR,
-          venue_phone VARCHAR,
-          venue_latitude FLOAT,
-          venue_longitude FLOAT
+        CREATE TABLE quiz (
+          quiz_id INT AUTO_INCREMENT PRIMARY KEY,
+          quiz_type VARCHAR(255) NOT NULL
         );`);
     })
     .then(() => {
       return db.query(`
-        CREATE TABLE teams (
-          team_id SERIAL PRIMARY KEY,
-          team_name VARCHAR NOT NULL,
-          venue_id INT REFERENCES venues(venue_id) NOT NULL,
-          team_start_time VARCHAR NOT NULL,
-          team_division VARCHAR NOT NULL
+        CREATE TABLE anoption (
+          anoption_id INT AUTO_INCREMENT PRIMARY KEY,
+          anoption_text VARCHAR(255) NOT NULL
         );`);
     })
     .then(() => {
       return db.query(`
-        CREATE TABLE users (
-          user_id SERIAL PRIMARY KEY,
-          team_id INT REFERENCES teams(team_id) NOT NULL,
-          user_name VARCHAR NOT NULL,
-          user_address_1 VARCHAR,
-          user_address_2 VARCHAR,
-          user_postcode VARCHAR,
-          user_dob DATE,
-          user_phone VARCHAR,
-          user_email VARCHAR NOT NULL,
-          user_password VARCHAR NOT NULL
-        )
-      `);
+        CREATE TABLE apostrophes (
+          apostrophe_id INT AUTO_INCREMENT PRIMARY KEY,
+          apostrophe_phrase VARCHAR(255) NOT NULL,
+          apostrophe_correct VARCHAR(255) NOT NULL,
+          apostrophe_wrong1 VARCHAR(255) NOT NULL,
+          apostrophe_wrong2 VARCHAR(255)
+        );`);
     })
     .then(() => {
       return db.query(`
         CREATE TABLE roles (
-          user_id INT PRIMARY KEY REFERENCES users(user_id) NOT NULL,
-          player_bool BOOL,
-          sec_bool BOOL,
-          umpire_bool BOOL,
-          organiser_bool BOOL
+          role_id INT AUTO_INCREMENT PRIMARY KEY,
+          role_title VARCHAR(255) NOT NULL
       )
     `);
     })
     .then(() => {
       return db.query(`
-        CREATE TABLE fixtures (
-          fixture_id SERIAL PRIMARY KEY,
-          match_status VARCHAR NOT NULL,
-          team1_id INT REFERENCES teams(team_id) NOT NULL,
-          team2_id INT REFERENCES teams(team_id) NOT NULL,
-          team1_score INT NOT NULL,
-          team2_score INT NOT NULL,
-          match_venue INT REFERENCES venues(venue_id) NOT NULL,
-          match_date DATE NOT NULL
+        CREATE TABLE users (
+          user_id INT AUTO_INCREMENT PRIMARY KEY,
+          user_role INT NOT NULL,
+          FOREIGN KEY (user_role) REFERENCES roles(role_id),
+          user_name VARCHAR(255) NOT NULL,
+          user_password VARCHAR(255)
         )
       `);
     })
     .then(() => {
       return db.query(`
-      CREATE TABLE responses (
-        response_id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(user_id) NOT NULL,
-        fixture_id INT REFERENCES fixtures(fixture_id) NOT NULL,
-        response VARCHAR NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP
+        CREATE TABLE attempts (
+          attempt_id INT AUTO_INCREMENT PRIMARY KEY,
+          attempt_user_id INT NOT NULL,
+          FOREIGN KEY (attempt_user_id) REFERENCES users(user_id),
+          attempt_quiz_id INT NOT NULL,
+          FOREIGN KEY (attempt_quiz_id) REFERENCES quiz(quiz_id),
+          attempt_created_at TIMESTAMP DEFAULT NOW(),
+          attempt_question_count INT NOT NULL,
+          attempt_correct_count INT NOT NULL
+        )
+      `);
+    })
+    .then(() => {
+      return db.query(`
+      CREATE TABLE aoran (
+        aoran_id INT AUTO_INCREMENT PRIMARY KEY,
+        aoran_answer INT NOT NULL,
+        FOREIGN KEY (aoran_answer) REFERENCES anoption(anoption_id),
+        aoran_question VARCHAR(255) NOT NULL
       )
       `);
     })
     .then(() => {
-      const insertVenuesQueryStr = format(
-        `INSERT INTO venues (venue_name, venue_address_1, venue_address_2, venue_postcode, venue_phone, venue_latitude, venue_longitude) VALUES %L`,
-        venuesData.map(
-          ({
-            venue_name,
-            venue_address_1,
-            venue_address_2,
-            venue_postcode,
-            venue_phone,
-            venue_latitude,
-            venue_longitude,
-          }) => [
-            venue_name,
-            venue_address_1,
-            venue_address_2,
-            venue_postcode,
-            venue_phone,
-            venue_latitude,
-            venue_longitude,
-          ]
-        )
+      const insertQuizQueryStr = mysql.format(
+        `INSERT INTO quiz (quiz_id, quiz_type) VALUES ?`,
+        [quizData.map(({ quiz_id, quiz_type }) => [quiz_id, quiz_type])]
       );
-      return db.query(insertVenuesQueryStr);
+      return db.query(insertQuizQueryStr);
     })
     .then(() => {
-      const insertTeamsQueryStr = format(
-        `INSERT INTO teams (team_name, venue_id, team_start_time, team_division) VALUES %L`,
-        teamsData.map(
-          ({ team_name, venue_id, team_start_time, team_division }) => [
-            team_name,
-            venue_id,
-            team_start_time,
-            team_division,
-          ]
-        )
+      const insertApostrophesQueryStr = mysql.format(
+        `INSERT INTO apostrophes (apostrophe_id, apostrophe_phrase, apostrophe_correct, apostrophe_wrong1, apostrophe_wrong2) VALUES ?`,
+        [
+          apostrophesData.map(
+            ({
+              apostrophe_id,
+              apostrophe_phrase,
+              apostrophe_correct,
+              apostrophe_wrong1,
+              apostrophe_wrong2,
+            }) => [
+              apostrophe_id,
+              apostrophe_phrase,
+              apostrophe_correct,
+              apostrophe_wrong1,
+              apostrophe_wrong2,
+            ]
+          ),
+        ]
       );
-      return db.query(insertTeamsQueryStr);
+      return db.query(insertApostrophesQueryStr);
     })
     .then(() => {
-      const insertUsersQueryStr = format(
-        `INSERT INTO users (team_id, user_name, user_address_1, user_address_2, user_postcode, user_dob, user_phone, user_email, user_password) VALUES %L`,
-        usersData.map(
-          ({
-            team_id,
-            user_name,
-            user_address_1,
-            user_address_2,
-            user_postcode,
-            user_dob,
-            user_phone,
-            user_email,
-            user_password,
-          }) => [
-            team_id,
-            user_name,
-            user_address_1,
-            user_address_2,
-            user_postcode,
-            user_dob,
-            user_phone,
-            user_email,
-            user_password,
-          ]
-        )
-      );
-      return db.query(insertUsersQueryStr);
-    })
-    .then(() => {
-      const insertRolesQueryStr = format(
-        `INSERT INTO roles (user_id, player_bool, sec_bool, umpire_bool, organiser_bool) VALUES %L`,
-        rolesData.map(
-          ({ user_id, player_bool, sec_bool, umpire_bool, organiser_bool }) => [
-            user_id,
-            player_bool,
-            sec_bool,
-            umpire_bool,
-            organiser_bool,
-          ]
-        )
+      const insertRolesQueryStr = mysql.format(
+        `INSERT INTO roles (role_id, role_title) VALUES ?`,
+        [rolesData.map(({ role_id, role_title }) => [role_id, role_title])]
       );
       return db.query(insertRolesQueryStr);
     })
     .then(() => {
-      const insertFixturesQueryStr = format(
-        `INSERT INTO fixtures (match_status, team1_id, team2_id, team1_score, team2_score, match_venue, match_date) VALUES %L`,
-        fixturesData.map(
-          ({
-            match_status,
-            team1_id,
-            team2_id,
-            team1_score,
-            team2_score,
-            match_venue,
-            match_date,
-          }) => [
-            match_status,
-            team1_id,
-            team2_id,
-            team1_score,
-            team2_score,
-            match_venue,
-            match_date,
-          ]
-        )
+      const insertUsersQueryStr = mysql.format(
+        `INSERT INTO users (user_id, user_name, user_password, user_role) VALUES ?`,
+        [
+          usersData.map(({ user_id, user_name, user_password, user_role }) => [
+            user_id,
+            user_name,
+            user_password,
+            user_role,
+          ]),
+        ]
       );
-      return db.query(insertFixturesQueryStr);
+      return db.query(insertUsersQueryStr);
     })
     .then(() => {
-      const insertResponsesQueryStr = format(
-        `INSERT INTO responses (user_id, fixture_id, response) VALUES %L`,
-        responsesData.map(({ user_id, fixture_id, response }) => [
-          user_id,
-          fixture_id,
-          response,
-        ])
+      const insertAttemptsQueryStr = mysql.format(
+        `INSERT INTO attempts (attempt_id, attempt_user_id, attempt_quiz_id, attempt_created_at, attempt_question_count, attempt_correct_count) VALUES ?`,
+        [
+          attemptsData.map(
+            ({
+              attempt_id,
+              attempt_user_id,
+              attempt_quiz_id,
+              attempt_created_at,
+              attempt_question_count,
+              attempt_correct_count,
+            }) => [
+              attempt_id,
+              attempt_user_id,
+              attempt_quiz_id,
+              attempt_created_at,
+              attempt_question_count,
+              attempt_correct_count,
+            ]
+          ),
+        ]
       );
-      return db.query(insertResponsesQueryStr);
+      return db.query(insertAttemptsQueryStr);
+    })
+    .then(() => {
+      const insertAnOptionQueryStr = mysql.format(
+        `INSERT INTO anoption (anoption_id, anoption_text) VALUES ?`,
+        [
+          anoptionData.map(({ anoption_id, anoption_text }) => [
+            anoption_id,
+            anoption_text,
+          ]),
+        ]
+      );
+      return db.query(insertAnOptionQueryStr);
+    })
+    .then(() => {
+      const insertAorAnQueryStr = mysql.format(
+        `INSERT INTO aoran (aoran_id, aoran_answer, aoran_question) VALUES ?`,
+        [
+          aoranData.map(({ aoran_id, aoran_answer, aoran_question }) => [
+            aoran_id,
+            aoran_answer,
+            aoran_question,
+          ]),
+        ]
+      );
+      return db.query(insertAorAnQueryStr);
     });
 };
 
